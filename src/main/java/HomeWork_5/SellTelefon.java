fca2c10b12a0d33223243418455d1f07eff3c66d
DimaGrishin <amvlastik@gmail.com>
2023-04-17 17:57:58 +0300

    дз 5.

---
+++ src/main/java/HomeWork_5/SellTelefon.java
@@ -0,0 +1,11 @@
+package HomeWork_5;
+
+import java.util.ArrayList;
+
+public interface SellTelefon {
+    ArrayList<Telefon> create(ArrayList<Telefon> listTelefon);
+
+    ArrayList<Telefon> sell(ArrayList<Telefon> listTelefon);
+
+    ArrayList<Telefon> addPhone(ArrayList<Telefon> listTelefon);
+}
